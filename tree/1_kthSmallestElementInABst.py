738c6b5bdc696e72ddade8caf2e3e4407af8e08d
Cheng Chin Kheng <chinkheng_98@hotmail.com>
2023-09-25 22:33:08 +0800

    tree: 1_kthSmallestElementInABst.py, 2nd solution

--- tree/1_kthSmallestElementInABst.py
+++ tree/1_kthSmallestElementInABst.py
@@ -24,3 +24,20 @@ class Solution:
 
         result = sortedDfs(root)
         return result[k-1]
+
+    # def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+    #     tree_stack = []
+    #     curr = root
+    #     index = 0 #number of sorted nums
+
+
+    #     while curr or tree_stack:
+    #         while curr: # go all the way to the left most node (smallest val)
+    #             tree_stack.append(curr) # append all the node along the way
+    #             curr = curr.left
+
+    #         curr = tree_stack.pop() # start processing from the leftmost node
+    #         index+=1
+    #         if(index==k):
+    #             return curr.val
+    #         curr = curr.right # process the right nodes starting from leftmost nodes
