2bc0acb73bcdf36829ea0afa4aec1cf68d499f50
ChengCK18 <chinkheng_98@hotmail.com>
2023-10-02 23:10:48 +0800

    Tree: deserialize in prog2, 2_SerializeandDeserializeBinaryTree

--- tree/2_SerializeandDeserializeBinaryTree.py
+++ tree/2_SerializeandDeserializeBinaryTree.py
@@ -13,8 +13,11 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
+        if(not root):
+            return 'null'
         tree_queue = deque([root])
         encoded = []
+    
         while tree_queue:
             for x in range(len(tree_queue)):
                 node = tree_queue.popleft()
@@ -27,50 +30,59 @@ class Codec:
                 tree_queue.append(node.right)
 
        
-            for x in range(len(encoded)-1,-1,-1): #to remove trailing None, which are not necessary
-                if(encoded[x]!='null'):
-                    break
-                else:
-                    encoded.pop()
-        
+        for x in range(len(encoded)-1,-1,-1): #to remove trailing None, which are not necessary
+            if(encoded[x]!='null'):
+                break
+            else:
+                encoded.pop()
         result = ','.join(encoded)
         return result
+    
 
-    def deserialize(self, data): ####### IN PROGRESS ####
+    #deserialize FAIL: [4,-7,-3,null,null,-9,-3,9,-7,-4,null,6,null,-6,-6,null,null,0,6,5,null,9,null,null,-1,-4,null,null,null,-2]
+    def deserialize(self, data): # child slice must be based on number of non null parents. To be modified.
         """Decodes your encoded data to tree.
         
         :type data: str
         :rtype: TreeNode
         """
         
+  
+        if(data == 'null'):
+            return []
+
         decoded = data.split(',')
         decoded_tree = TreeNode(decoded[0])
         level = 1
         parent = deque([decoded_tree])
-        node = None
         while True:
-            child_slice = decoded[(2**level)-1:(2**level)-1 +(2**level)]
+            level = len(parent)
+            child_slice = decoded[(2**level)-1:(2**level)-1 +level]
             if(len(child_slice)==0):
                 break
-
+            # print(child_slice)
             parent_index = 0
             temp = deque()
             while parent:
                 node = parent.popleft()
-                print(child_slice[2*parent_index])
-                print(child_slice[2*parent_index+1])
-                node.left = TreeNode(child_slice[2*parent_index])
-                node.right = TreeNode(child_slice[2*parent_index+1])
+                if(not node):
+                    continue
+                if((2*parent_index)<len(child_slice) and child_slice[2*parent_index] != 'null'):
+                    node.left = TreeNode(child_slice[2*parent_index])
+                if((2*parent_index+1)<len(child_slice) and child_slice[2*parent_index+1] != 'null'):
+                    node.right = TreeNode(child_slice[2*parent_index+1])
                 temp.append(node.left)
                 temp.append(node.right)
                 parent_index +=1
-                print(node)
+                # print(node)
+            
             parent = temp
+            print(len(parent))
 
-            # print(node)
+            
             level+=1
-        
-        # print(decoded_tree)
+       
+        return decoded_tree
         
 
 
