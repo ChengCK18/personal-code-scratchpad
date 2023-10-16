9405981fa5f6986b77194794ae4c1e24c11761a2
ChengCK18 <chinkheng_98@hotmail.com>
2023-10-16 20:55:31 +0800

    2_implementTriePrefixTree.py dict approach. Much simpler codes

--- tries/2_implementTriePrefixTree.py
+++ tries/2_implementTriePrefixTree.py
@@ -1,3 +1,58 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if(c not in cur.children):
+                cur.children[c] = Node()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            if(c not in cur.children):
+                return False
+            cur = cur.children[c]
+        return cur.endOfWord # has to end with valid word
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            if(c not in cur.children):
+                return False
+            cur = cur.children[c]
+     
+       
+        if(len(cur.children.keys())>0 or cur.endOfWord == True):
+            return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+
+
+
+
+
+
+
+
 ########## 
 # Initial attempt, success 
 # but can be improved to use dict instead of list to make lookup much faster
