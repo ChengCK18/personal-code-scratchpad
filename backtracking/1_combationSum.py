e521bb80051dcfea5ca2da2e1f490a0dec49ae15
ChengCK18 <chinkheng_98@hotmail.com>
2023-11-14 23:28:46 +0800

    backtracking: 1_combinationSum.py sol 2

--- backtracking/1_combationSum.py
+++ backtracking/1_combationSum.py
@@ -34,3 +34,31 @@ class Solution:
             
 
         return output
+    
+
+
+
+#### APPROACH 2 #####
+
+# def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    # output=[]
+
+
+
+    # def dfs(i,curr,total):
+    #     if(total == target):
+    #         output.append(curr.copy())
+    #         return
+
+    #     if(total > target or i >=len(candidates)):
+    #         return
+
+    #     curr.append(candidates[i])
+
+    #     dfs(i,curr, total+candidates[i])
+    #     curr.pop()
+    #     dfs(i+1,curr, total)
+
+    # dfs(0,[],0)
+
+    # return output
