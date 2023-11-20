452ab58b2b98357b535cbc56a9ce9c093db39d32
ChengCK18 <chinkheng_98@hotmail.com>
2023-11-20 22:26:39 +0800

    Graphs: 1_numberOfIslands.py completed(bfs-approach)

--- graphs/1_numberOfIslands.py
+++ graphs/1_numberOfIslands.py
@@ -1,4 +1,4 @@
-#Working non-graph based approach :D
+#:D Working dfs based approach 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
@@ -43,6 +43,46 @@ class Solution:
 
         return totalIsland
 
+
+
+# bfs-based sol, slower than dfs above
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+        totalIsland = 0
+
+        def bfs(r,c):
+            que = collections.deque()
+            visited.add((r,c))
+            que.append((r,c))
+
+            while que:
+                rw,cl = que.popleft()
+
+                # check its surrounding top,bot,left,right
+                directions = [[0,1],[0,-1],[-1,0],[1,0]]
+                for direc in directions:
+                    addedRw = rw + direc[0]
+                    addedCl = cl + direc[1]
+                    if(addedRw in range(ROWS) and addedCl in range(COLS) and 
+                    grid[addedRw][addedCl]=="1" and (addedRw,addedCl) not in visited):
+                        que.append((addedRw,addedCl))
+                        visited.add((addedRw,addedCl))
+
+
+
+
+        for rw in range(ROWS):
+            for cl in range(COLS):
+                if((rw,cl) not in visited and grid[rw][cl]=="1"):
+                    bfs(rw,cl)
+                    totalIsland +=1
+
+        return totalIsland
 
         
 
