9b540b6d09e3fd5da15195b16cb189ada3497a91
ChengCK18 <chinkheng_98@hotmail.com>
2023-11-27 22:22:06 +0800

    Graphs: 1_1_pacificAtlanticWaterFlow, solution also TLE, to optimize further

--- graphs/1_pacificAtlanticWaterFlow.py
+++ graphs/1_pacificAtlanticWaterFlow.py
@@ -1,3 +1,70 @@
+
+#still TLE, 111/113. Need further optimization or diff algo
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        # to be true
+        # pacific =  0 in (x and/or y)
+        # atlantic = len(m)-1 or len(n)-1 in (x and/or y)
+        ROWS, COLS = len(heights),len(heights[0])
+        finalResult = []
+        main_visited = {}
+        def bfsFlowFromOrigin(rw,cl):
+            visited = set()
+            reachPacific,reachAtlantic = False, False
+            que = collections.deque()
+            que.append((rw,cl))
+
+            while que:
+                row,col = que.popleft()
+                visited.add((row,col))
+                if(row == 0 or col == 0):
+                    reachPacific = True
+                if(row == ROWS-1 or col == COLS-1):
+                    reachAtlantic = True
+
+                if(reachPacific and reachAtlantic): # no point continuing, both cond fulfilled
+                    return True 
+        
+                currNodeVal = heights[row][col]
+                direction = [[1,0],[-1,0],[0,1],[0,-1]] # north, south, east, west
+
+                for direc in direction:
+                    addedRw = row+direc[0]
+                    addedCol = col+direc[1]
+
+                    if((addedRw,addedCol) in main_visited and
+                    heights[addedRw][addedCol]<=currNodeVal
+                    ):
+                        reachPacific = main_visited[(addedRw,addedCol)][0] or reachPacific
+                        reachAtlantic = main_visited[(addedRw,addedCol)][1] or reachAtlantic
+                        continue
+                    if(
+                        addedRw in range(ROWS) and
+                        addedCol in range(COLS) and 
+                        heights[addedRw][addedCol]<=currNodeVal and 
+                        (addedRw,addedCol) not in visited  
+                        ):
+
+                        que.append((addedRw,addedCol))
+
+
+            main_visited[(rw,cl)] = (reachPacific,reachAtlantic)
+            return reachPacific and reachAtlantic
+
+
+        for rw in range(ROWS):
+            for cl in range(COLS):
+                result = bfsFlowFromOrigin(rw,cl)
+                if(result):
+                    finalResult.append([rw,cl])
+
+       
+        return finalResult
+
+
+
+
+
 #TLE, 111/113. Need optimization
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
