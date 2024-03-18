8f830a4d744c7373bd66698f3f27f53ba2a9bffe
ChengCK18 <chinkheng_98@hotmail.com>
2024-03-18 20:50:24 +0800

    1d_dynamicProgramming: 1_houseRobber2 done :D

---
+++ 1d_dynamicProgramming/1_houseRobber2.py
@@ -0,0 +1,24 @@
+# Initial accepted own attempt. Not a fast algo according to benchmark
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 0:
+            return 0
+        
+        if len(nums)==1:
+            return nums[0]
+
+
+        path1A, path2A = 0,0
+        for n in nums[:-1]:
+            temp = max(n+path1A,path2A)
+            path1A = path2A
+            path2A = temp
+            
+        path1B, path2B = 0,0
+        for n in nums[1:]:
+            temp = max(n+path1B,path2B)
+            path1B = path2B
+            path2B = temp
+
+        return max(path2A,path2B)
